da7e0df69a2590007f6ca00bdbde833c50eeed44
Rased <rased@dohatec.net>
2022-09-27 14:28:58 +0600

    updated

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 from steganogan import SteganoGAN
+from multiprocessing import Pool, freeze_support, cpu_count
 
 input_path = 'research\\data\\div2k\\val\\_'
 output_path = 'research\\StegImages'
@@ -12,10 +13,18 @@ files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
 
 steganogan = SteganoGAN.load(architecture='dense', cuda=False)
 # steganogan = SteganoGAN.load(path='research/models/1664212615/weights.steg', cuda=False)
+# steganogan.encode('research/input.png', 'research/output.png', 'This is a super secret message!')
 
-for file in files:
+def process(file):
     input_file_path = os.path.join(input_path, file)
     output_file_path = os.path.join(output_path, file)
     print('processing ' + file)
     # steganogan.encode('research/input.png', 'research/output.png', 'This is a super secret message!')
     steganogan.encode(input_file_path, output_file_path, 'This is a super secret message!')
+
+if __name__ == '__main__':
+    freeze_support()
+    cpu_count = int(cpu_count() * 0.75)
+    print('No of CPU cores: ' + str(cpu_count))
+    pool = Pool(cpu_count)
+    pool.map(process, files)
